c93f98bc67275cb2fa35127a91fd0d6dbf832c02
SofiaRin <bujie02@sina.com>
2017-03-28 18:02:56 +0800

    Finish Homework 6

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,22 @@
-window.onload = () => {
+var canvas = document.getElementById("app") as HTMLCanvasElement;
 
-    let canvas = document.getElementById("app") as HTMLCanvasElement;
-    let stage = engine.run(canvas);
-    let bitmap = new engine.BitMap();
-    let image = document.createElement("img");
-    image.src = "wander-icon.jpg";
-    bitmap.image = image;
-    stage.addChild(bitmap);
+var stage = engine.run(canvas);
+var bitmap = createBitmapByName("S_Setuper.png");
 
-}
+
+stage.addChild(bitmap);
+let speed = 10;
+
+engine.Ticker.getInstance().register((deltaTime) => {
+    console.log("aaa")
+    bitmap.x += 1;
+});
+
+
+function createBitmapByName(name: string): engine.BitMap {
+        var result = new engine.BitMap();
+        //var texture: engine.Texture = RES.getRes(name);
+      //  result.texture = texture;
+      result.src = "assets/" + name;
+        return result;
+    }
